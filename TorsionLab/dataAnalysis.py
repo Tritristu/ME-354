53c8173ec6e1dd0f0aef58f978eb0eadd9c51997
Tritristu <Tritristu@users.noreply.github.com>
2023-02-24 15:14:21 -0800

    theoretical plots work!

--- TorsionLab/dataAnalysis.py
+++ TorsionLab/dataAnalysis.py
@@ -41,44 +41,42 @@ for File in Files:
 for File in Files:
     Data[File]['Force (N)'] = Data[File]['Force  (kgf)']*g #this adds force in Newtons to the data
     Data[File]['Torque (N.m)'] = Data[File]['Force (N)']*(213/85)*gripeDia #this adds applied torque to the data
-    Data[File]['Angle (rad)'] = Data[File]['Angle (deg) ']*(np.pi/180) #this adds applied torque to the data
+    Data[File]['Angle (rad)'] = Data[File]['Angle (deg) ']*(np.pi/180) #this adds angles in radians
 
 # Torque Vs Twist Graph
 fig = plt.figure(1)
 ax = fig.gca()
 for File in Aluminum:
-    ax.plot(Data[File]['Angle (rad)'],Data[File]['Torque (N.m)'],label=File[:len(File)-4] + ' Torque')
+    ax.plot(Data[File]['Angle (deg) '],Data[File]['Torque (N.m)'],label=File[:len(File)-4] + ' Torque')
 ax.set_xlim(left = 0)
 ax.set_ylim(bottom = 0)
 plt.title("Aluminum Torque vs Twist Angle")
 plt.ylabel('Torque (N.m)')
-plt.xlabel('Twist Angle (rad)')
+plt.xlabel('Twist Angle (deg)')
 plt.legend()
 
 fig = plt.figure(2)
 ax = fig.gca()
 for File in Steel:
-    ax.plot(Data[File]['Angle (rad)'],Data[File]['Torque (N.m)'],label=File[:len(File)-4] + ' Torque')
+    ax.plot(Data[File]['Angle (deg) '],Data[File]['Torque (N.m)'],label=File[:len(File)-4] + ' Torque')
 ax.set_xlim(left = 0)
 ax.set_ylim(bottom = 0)
 plt.title("Steel Torque vs Twist Angle")
 plt.ylabel('Torque (N.m)')
-plt.xlabel('Twist Angle (rad)')
+plt.xlabel('Twist Angle (deg)')
 plt.legend()
 
 # Shear Modulus Calculation
 shearModuli = {}
-def shearFit(angle, torque, a, b):
-    nu, C, R, P, Err = linregress(torque[a:b], angle[a:b])  # The data outputs the slope (nu), intercept (C), regression (R) value, P-value and standard error
+def shearFit(strain, stress, a, b):
+    modulus, intercept, R, P, Err = linregress(strain[a:b], stress[a:b]) 
 
-    # Y = [0.0, torque[round(1.5 * b)]]
-    # X = [(y - C) / nu for y in Y]  # these are points that you can plot to visualize the data being fit, inverted from y=nu*x+C, x=(y-C)/nu
-    return nu
+    return modulus
 
 for File in Files:
     shearStress = Data[File]['Torque (N.m)']*rodDia/(2*momentOfInertia)
-    shearStrain = Data[File]['Angle (deg) ']*rodDia/(2*length)
-    shearModuli[File] = shearFit(shearStress,shearStrain,0,10)
+    shearStrain = Data[File]['Angle (rad)']*rodDia/(2*length)
+    shearModuli[File] = shearFit(shearStrain,shearStress,0,20)
 
 avgSteelShearMod = 0
 avgAluminumShearMod = 0
@@ -125,33 +123,45 @@ plt.xlabel('Twist Angle (deg)')
 plt.legend()
 
 # Calculating the theoretical angle-torque plot
-def theoreticalPlot(endpoint,yieldStrength,shearMod,hardenCoeff,hardenExp):
+def theoreticalPlot(endpoint,yieldStrength,elasticMod,poissons,hardenCoeff,hardenExp):
     length = 180e-3
     rodDia = 4.76e-3
-    thetaTrans = yieldStrength*length/(shearMod*rodDia)
-    elasticRegion = np.linspace(0,thetaTrans)
-    plasticRegion = np.linspace(thetaTrans,endpoint,num=100)
-    elasticTorque = np.pi*shearMod*rodDia*elasticRegion*(np.pi/180)/(64*length)
-    rodYield = (yieldStrength/2)*length/(shearMod*plasticRegion*(np.pi/180))
-    plasticTorque = ((2*np.pi*hardenCoeff)/((hardenExp+3)*np.sqrt(3)))*((plasticRegion*(np.pi/180)/(length*np.sqrt(3)))**3)*(rodDia**(hardenExp+3)-rodYield**(hardenExp+3))
-    angles = np.concatenate([elasticRegion,plasticRegion]) # having trouble concatenating these two lists
+    shearMod = elasticMod/(2 + 2*poissons)
+    thetaTrans = 2*yieldStrength*length/(shearMod*rodDia*np.sqrt(3)) # rad
+    elasticRegion = np.linspace(0,thetaTrans,num=200)
+    plasticRegion = np.linspace(thetaTrans,endpoint,num=200)
+    elasticTorque = np.pi*shearMod*(rodDia**4)*elasticRegion/((2**5)*length)
+    rodYield = yieldStrength*length/(2*shearMod*plasticRegion)
+    plasticTorque = (np.pi*yieldStrength*rodYield**3)/(2*np.sqrt(3)) + ((2*np.pi*hardenCoeff)/((hardenExp+3)*np.sqrt(3)))*((plasticRegion/(length*np.sqrt(3)))**hardenExp)*((rodDia/2)**(hardenExp+3) - rodYield**(hardenExp+3))
+    angles = np.concatenate([elasticRegion,plasticRegion])*(180/np.pi) # put back into degrees
     torque = np.concatenate([elasticTorque,plasticTorque])
     return angles,torque
 
-# anglesSteelTh, torqueSteelTh = theoreticalPlot(1000,steelYield,avgSteelShearMod,steelHardCoeff,steelHardExp)
-# anglesAluminumTh, torqueAluminumTh = theoreticalPlot(1600,aluminumYield,avgAluminumShearMod,aluminumHardCoeffNom,aluminumHardExpNom)
+anglesSteelTh, torqueSteelTh = theoreticalPlot(8500*(np.pi/180),steelYield,steelElasticMod,steelPoisson,steelHardCoeffNom,steelHardExpNom)
+anglesAluminumTh, torqueAluminumTh = theoreticalPlot(15500*(np.pi/180),aluminumYield,aluminumElasticMod,aluminumPoisson,aluminumHardCoeffNom,aluminumHardExpNom)
 
 fig = plt.figure(5)
 ax = fig.gca()
-# for File in Aluminum:
-#     ax.plot(Data[File]['Angle (deg) '],Data[File]['Torque (N.m)'],label=File[:len(File)-4]+' torque')
-# ax.plot(anglesAluminumTh,torqueAluminumTh,label='Theortical torque')
+for File in Aluminum:
+    ax.plot(Data[File]['Angle (deg) '],Data[File]['Torque (N.m)'],label=File[:len(File)-4]+' Torque')
+ax.plot(anglesAluminumTh,torqueAluminumTh,label='Theoretical torque')
 ax.set_xlim(left = 0)
-ax.set_ylim(bottom = 0)
+ax.set_ylim(bottom = 0, top = 7.5)
 plt.title("Aluminum Torque vs Twist Angle")
 plt.ylabel('Torque (N.m)')
 plt.xlabel('Twist Angle (deg)')
 plt.legend()
 
+fig = plt.figure(6)
+ax = fig.gca()
+for File in Steel:
+    ax.plot(Data[File]['Angle (deg) '],Data[File]['Torque (N.m)'],label=File[:len(File)-4]+' Torque')
+ax.plot(anglesSteelTh,torqueSteelTh,label='Theoretical torque')
+ax.set_xlim(left = 0)
+ax.set_ylim(bottom = 0, top = 15)
+plt.title("Steel Torque vs Twist Angle")
+plt.ylabel('Torque (N.m)')
+plt.xlabel('Twist Angle (deg)')
+plt.legend()
 
 plt.show()
